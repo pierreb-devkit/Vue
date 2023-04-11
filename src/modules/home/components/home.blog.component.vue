c0e65ffd3e46acbe81b66a2f55bc704426bfd1f6
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2023-04-11 20:11:55 +0200

    fix(blog): title color 🐛

--- src/modules/home/components/home.blog.component.vue
+++ src/modules/home/components/home.blog.component.vue
@@ -25,7 +25,7 @@
           <!-- eslint-disable-next-line -->
           <a :href="url" target="_blank">
             <v-img :src="feature_image" class="mb-4 rounded-xl" height="200" max-width="100%" cover></v-img>
-            <h3 class="mb-4 text-primary" v-text="title"></h3>
+            <h3 class="mb-4 text-secondary" v-text="title"></h3>
             <div class="subtitle-1 text-medium-emphasis text-onSurface">
               <v-markdown :source="excerpt" />
             </div>
@@ -41,7 +41,7 @@
         <v-col v-for="({ feature_image, excerpt, title, url }, i) in newsd" :key="i" cols="12" md="4" data-aos="fade-up">
           <a :href="url" target="_blank">
             <v-img :src="feature_image" class="mb-4 rounded-xl" height="200" max-width="100%" cover></v-img>
-            <h3 class="mb-4 text-primary" v-text="title"></h3>
+            <h3 class="mb-4 text-secondary" v-text="title"></h3>
             <div class="subtitle-1 text-medium-emphasis text-onSurface">
               <v-markdown :source="excerpt" />
             </div>
